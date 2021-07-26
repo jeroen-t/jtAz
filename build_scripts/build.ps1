974e548b9e5b350828a25c8603cba4cb0ec0bc25
Jeroen Trimbach <62345920+jeroen-t@users.noreply.github.com>
2021-07-26 20:29:40 +0200

    Update build.ps1

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -1,18 +1,12 @@
-$buildVersion = $env:BUILDVER
-$moduleName = 'jtAz'
-$manifestPath = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath "$moduleName.psd1"
+$Global:ErrorActionPreference = 'Stop'
+$Global:VerbosePreference = 'SilentlyContinue'
 
-## Update build version in manifest
-$manifestContent = Get-Content -Path $manifestPath -Raw
-$manifestContent = $manifestContent -replace '', $buildVersion
-
-## Find all of the public functions
-$publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'public'
-if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {     $funcStrings = "'$($publicFunctionNames -join "','")'"
-} else {
-    $funcStrings = $null
+# Prepare NuGet / PSGallery
+if (!(Get-PackageProvider | Where-Object { $_.Name -eq 'NuGet' })) {
+    Install-PackageProvider -Name NuGet -force | Out-Null
 }
+Import-PackageProvider -Name NuGet -force | Out-Null
 
-## Add all public functions to FunctionsToExport attribute
-$manifestContent = $manifestContent -replace "''", $funcStrings
-$manifestContent | Set-Content -Path $manifestPath
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
