15be7bb421e14321330be6c5590d45a30bbaf8a6
Jeroen Trimbach <Jeroen Trimbach>
2021-07-26 20:13:23 +0200

    directory

--- jtAz/jtAz.Test.ps1
+++ jtAz/jtAz.Test.ps1
@@ -2,14 +2,14 @@ Install-Module -Name PSScriptAnalyzer -Force
 
 describe 'Module-level tests' {
     it 'the module imports successfully' {
-        { Import-Module "$PSScriptRoot\jtAz.psm1" -ErrorAction Stop } | should -not throw
+        { Import-Module "$PSScriptRoot\jtAz\jtAz.psm1" -ErrorAction Stop } | should -not throw
     }
 
     it 'the module has an associated manifest' {
-        Test-Path "$PSScriptRoot\jtAz.psd1" | should -Be $true
+        Test-Path "$PSScriptRoot\jtAz\jtAz.psd1" | should -Be $true
     }
 
     it 'passes all default PSScriptAnalyzer rules' {
-        Invoke-ScriptAnalyzer -Path "$PSScriptRoot\jtAz.psm1" | should -BeNullOrEmpty
+        Invoke-ScriptAnalyzer -Path "$PSScriptRoot\jtAz\jtAz.psm1" | should -BeNullOrEmpty
     }
 }
